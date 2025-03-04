bb348755916670efe7e6a6ff0205d3f0bd5222e6
dfigueiredo31 <d.figueiredo@outlook.pt>
2025-03-04 19:22:00 +0000

    Update ao datamodel Order:
    OrderTotalPrice
    OrderTax
    OrderDate

--- WpfProjectLpdsw/WpfProjectLpdsw/Models/Order.cs
+++ WpfProjectLpdsw/WpfProjectLpdsw/Models/Order.cs
@@ -11,5 +11,8 @@ namespace WpfProjectLpdsw.Models
         public int OrderId { get; set; }
         public int ClientId { get; set; }
         public List<Product> OrderProducts { get; set; }
+        public float OrderTotalPrice { get; set; }
+        public float OrderTax {  get; set; }
+        public DateTime OrderDate { get; set; }
     }
 }
