050f14f2bd1735a8b8a9fc95c23b5650e3af3088
dfigueiredo31 <d.figueiredo@outlook.pt>
2025-03-04 17:58:55 +0000

    Adicionadas propriedades ao model do cliente

--- WpfProjectLpdsw/WpfProjectLpdsw/Models/Client.cs
+++ WpfProjectLpdsw/WpfProjectLpdsw/Models/Client.cs
@@ -8,5 +8,13 @@ namespace WpfProjectLpdsw.Models
 {
     internal class Client
     {
+        public int Id { get; set; }
+        public string ClientName {  get; set; }
+        public string ClientEmail { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+
     }
 }
